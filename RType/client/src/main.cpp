f836eb84d904984081a9fa4f55e5aa8ad239c0c6
Mathis Emaille <matlyce@gmail.com>
2024-11-26 22:15:00 +0100

    Edit: (client) main.cpp -> handle textures management (sprites); Add: clang formating; Add: Ships && Bullet assets
    Signed-off-by: Mathis Emaille <matlyce@gmail.com>

--- RType/client/src/main.cpp
+++ RType/client/src/main.cpp
@@ -1,11 +1,23 @@
 #include "../../../ECS/includes/ECS.hpp"
 #include "../includes/Components.hpp"
-#include "../includes/Systems.hpp"
+#include "../includes/TexturesManager.hpp"
+
+#include "../includes/Systems/ClientRelatives.hpp"
+#include "../includes/Systems/SpriteRelatives.hpp"
+#include "../includes/Systems/Generics.hpp"
+
+#include "../../shared/includes/Components/GameComponents.hpp"
+#include "../../shared/includes/Systems/Game.hpp"
 
 Coordinator gCoordinator;
 
 int main() {
-    InitWindow(800, 600, "RType");
+    const int WINDOW_WIDTH = 800;
+    const int WINDOW_HEIGHT = 600;
+    const float SHIP_HEIGHT = WINDOW_HEIGHT * 0.05f;
+    auto& TexturesManager = TexturesManager::getInstance();
+
+    InitWindow(WINDOW_WIDTH, WINDOW_HEIGHT, "RType");
     SetTargetFPS(60);
 
     gCoordinator.init();
@@ -13,11 +25,20 @@ int main() {
     gCoordinator.registerComponent<ShipComponent>();
     gCoordinator.registerComponent<BulletComponent>();
     gCoordinator.registerComponent<InputComponent>();
+    gCoordinator.registerComponent<TimerComponent>();
+    gCoordinator.registerComponent<SpriteComponent>();
+    gCoordinator.registerComponent<SpriteAnimationComponent>();
+    gCoordinator.registerComponent<SpriteFrameComponent>();
+
+    //? Shared components
+    gCoordinator.registerComponent<PhysicsComponent>();
 
     //* Systems
     auto renderSystem = gCoordinator.registerSystem<RenderSystem>();
     auto inputSystem = gCoordinator.registerSystem<InputSystem>();
     auto physicsSystem = gCoordinator.registerSystem<PhysicsSystem>();
+    auto timerSystem = gCoordinator.registerSystem<TimerSystem>();
+    auto spriteFrameSystem = gCoordinator.registerSystem<SpriteFrameSystem>();
     Signature signature;
 
     //? RenderSystem
@@ -36,17 +57,50 @@ int main() {
     signature.set(gCoordinator.getComponentTypeID<BulletComponent>(), true);
     gCoordinator.setSystemSignature<PhysicsSystem>(signature);
 
+    //? TimerSystem
+    signature.reset();
+    signature.set(gCoordinator.getComponentTypeID<TimerComponent>(), true);
+    gCoordinator.setSystemSignature<TimerSystem>(signature);
+
+    //? SpriteFrameSystem
+    signature.reset();
+    signature.set(gCoordinator.getComponentTypeID<SpriteFrameComponent>(), true);
+    gCoordinator.setSystemSignature<SpriteFrameSystem>(signature);
+
     //? entity
     Entity ship = gCoordinator.createEntity();
-    gCoordinator.addComponent(ship, TransformComponent({400, 500}));
     gCoordinator.addComponent(ship, ShipComponent());
     gCoordinator.addComponent(ship, InputComponent());
+    gCoordinator.addComponent(ship, TimerComponent());
+
+    std::string shipTexturePath = "../assets/textures/ships/ship_1.png";
+    Texture2D shipTexture = TexturesManager.loadTexture(shipTexturePath);
+    Rectangle initialFrame = TexturesManager.getFrame(shipTexturePath, 2, 5);
+
+    gCoordinator.addComponent(ship, SpriteComponent(
+        shipTexture,
+        initialFrame
+    ));
+
+    float frameWidth = (float)(shipTexture.width / 5);
+    float frameAspectRatio = frameWidth / shipTexture.height;
+    float SHIP_WIDTH = SHIP_HEIGHT * frameAspectRatio;
+
+    gCoordinator.addComponent(ship, TransformComponent(
+        Vector2{WINDOW_WIDTH / 2.0f, WINDOW_HEIGHT * 0.8f},
+        0.0f,
+        Vector2{1.0f, 1.0f},
+        Vector2{SHIP_WIDTH, SHIP_HEIGHT}
+    ));
+    gCoordinator.addComponent(ship, SpriteFrameComponent(1, 5));
 
     //! MAIN LOOP
     while (!WindowShouldClose()) {
         //? LOGIC
         inputSystem->update();
-        physicsSystem->update();
+        spriteFrameSystem->update();
+        timerSystem->update();
+        physicsSystem->update(GetFrameTime());
 
         //? RENDER
         BeginDrawing();
