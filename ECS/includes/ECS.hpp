baeecfbac0a91584f4f836633d095f8fb41a40b6
Mathis Emaille <matlyce@gmail.com>
2024-11-26 15:29:33 +0100

    Add: ECS mvp version
    Signed-off-by: Mathis Emaille <matlyce@gmail.com>

---
+++ ECS/includes/ECS.hpp
@@ -0,0 +1,12 @@
+#pragma once
+
+#include "Types.hpp"
+#include "EntityManager.hpp"
+#include "AComponentStorage.hpp"
+#include "ComponentStorage.hpp"
+#include "ComponentManager.hpp"
+#include "System.hpp"
+#include "SystemManager.hpp"
+#include "Coordinator.hpp"
+
+extern Coordinator gCoordinator;
