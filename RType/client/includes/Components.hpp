df007c2b386a195fd4bb124302c61c073a810132
Mathis Emaille <matlyce@gmail.com>
2024-11-26 15:31:02 +0100

    Add: client simple implementation (usage) of the ECS
    Signed-off-by: Mathis Emaille <matlyce@gmail.com>

---
+++ RType/client/includes/Components.hpp
@@ -0,0 +1,20 @@
+#pragma once
+#include <raylib.h>
+
+struct TransformComponent {
+    Vector2 position;
+    float rotation;
+    Vector2 scale;
+
+    TransformComponent(Vector2 pos = {0, 0}, float rot = 0.0f, Vector2 scl = {1, 1})
+        : position(pos), rotation(rot), scale(scl) {}
+};
+
+struct ShipComponent {};
+
+struct BulletComponent {
+    Vector2 velocity;
+};
+
+struct InputComponent {};
+struct ClientNetwork {};
